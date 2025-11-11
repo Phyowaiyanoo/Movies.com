80ea2aa76b1f1763bdefba30540bc2917ff0df55
Phyowaiyanoo <163579372+Phyowaiyanoo@users.noreply.github.com>
2025-11-11 13:53:30 -0800

    Update Store.js

--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -1,9 +1,5 @@
-
-import { createStore } from "redux";
-import reducers from "../Reducer/Reducer";
-
+ import {createStore} from 'redux'
+import reducers from '../Reducer/Reducer'
  export const store = createStore(
-    reducers, {},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-);
-//https://api.themoviedb.org/3/movie/now_playing?api_key=39811d5fa219d9fe38cd2956c20a5f98
+   reducers , { },  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ )
