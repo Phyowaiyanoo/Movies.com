bdb24a8d7dcbe228fb3c6ff5c7a9c4bf5306bf49
Phyowaiyanoo <163579372+Phyowaiyanoo@users.noreply.github.com>
2025-11-11 13:52:19 -0800

    Update Reducer.js

--- src/Redux/Reducer/Reducer.js
+++ src/Redux/Reducer/Reducer.js
@@ -1,8 +1,8 @@
-import { combineReducers } from "redux";
-import { movieReducer } from "./Movie";
+import {combineReducers}from "redux"
+import { movieReducer } from "./movies/reducer"
 
-const reducers = combineReducers(
-    {movies : movieReducer}
+const  reducers = combineReducers(
+{ movies : movieReducer
+}
 )
-
 export default reducers;
