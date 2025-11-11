1947cf85a6526c688b625df1e5a08727aa029957
Phyowaiyanoo <163579372+Phyowaiyanoo@users.noreply.github.com>
2025-11-11 13:40:09 -0800

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,57 +1,45 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react'
-import "../CSS/Home.css"
-import { api, api_key } from '../Api'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchMovies } from '../Redux/Action/Movies'
-import { Link} from 'react-router'
-import Search from './Search'
-
+import { api, api_key } from '../Api/Axios';
+import { fetchMovie } from '../Redux/Action/Movies';
+import "../CSS/Home.css"
+import { Link } from 'react-router-dom';
+import Search from './Search';
 const Home = () => {
-  const dispatch = useDispatch()
-  const getMovies = async () => {
-    const res = await api.get(`/movie/now_playing?api_key=${api_key}`)
-    dispatch(fetchMovies(res.data.results))
-  }
-  useEffect(() => {
-    getMovies()
-  }, [])
-
-  let movies = [];
-  movies = useSelector((state) => state.movies.movies)
-
+    const dispatch = useDispatch();
+    const getMovie = async ()=>{
+        const res = await api.get(`/movie/popular?api_key=${api_key}`)
+         dispatch(fetchMovie(res.data.results));
+           }
+    useEffect(()=>{
+       getMovie()
+    },[]);
+    let movie = {};
+    movie = useSelector((state)=>state.movies.movies)
+    
   return (
     <>
-      <Search />
+    <Search/>
+    <div className='fetching'>
 
-      <div className='photo'>
+      { movie.length > 0 ? movie.map((movie)=>
 
-
-        {
-          movies.length > 0 ? movies.map(movie => (
-
-            <div key={movie.id} className='div_2'>
-              <Link to = {`/Details/${movie.id}`} style={{color:"aliceblue" , textDecoration:"none"}}>
-                <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt="movies" />
-
-              <h1>{movie.title}</h1>
-
-              <p>{movie.overview.slice(0, 90)}</p>
-
-              <i className="fa-solid fa-star"></i> {movie.vote_average}
-              </Link>
-           
-            </div>
-          )) : <div className='loading'></div>
-
-        }
-
-
-      </div>
+          <div className='photo' key={movie.id}>
+           <Link to = {`/Details/${movie.id}`} className='link'>
+            <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt='images'/>
+            <h1>{movie.title}</h1>
+            <p>{movie.overview.slice(0,100)}</p>
+            <h2> <i className="fa-solid fa-calendar-days"></i> {movie.release_date}</h2>
+            <h2> <i className="fa-solid fa-star-of-life"></i> {movie.vote_average}</h2>
+           </Link>
+          </div>)   
+        : <div className='loading'></div>
+       }
+    </div>
     </>
   )
 }
-
 
 export default Home
