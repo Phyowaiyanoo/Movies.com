aae01244b7d9a2574a593b341c2f6255b970c2f2
Phyowaiyanoo <163579372+Phyowaiyanoo@users.noreply.github.com>
2025-11-11 13:36:03 -0800

    Update index.js

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
+export const api_key="39811d5fa219d9fe38cd2956c20a5f98"
 
-export const api_key="39811d5fa219d9fe38cd2956c20a5f98";
-
-export const api = axios.create({
+ export const api = axios.create({
     baseURL : "https://api.themoviedb.org/3"
-})
+ });
