ab760dae0deb056df894c792d74df75065c1fb2e
Phyowaiyanoo <163579372+Phyowaiyanoo@users.noreply.github.com>
2025-11-11 13:39:18 -0800

    Update Details.jsx

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,57 +1,50 @@
-/* eslint-disable eqeqeq */
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react'
+import { api, api_key } from '../Api/Axios'
 import { useDispatch, useSelector } from 'react-redux'
-import { api, api_key } from '../Api'
-import { useParams , NavLink } from 'react-router'
 import { removeMovie, selectMovie } from '../Redux/Action/Movies'
+import { NavLink, useParams } from 'react-router-dom'
 import "../CSS/Details.css"
 const Details = () => {
+  const {id} = useParams()
   const dispatch = useDispatch()
-  const { id } = useParams()
 
-  const getDetails = async () => {
+  const detailsMovie =async ()=>{
+ 
+
     const res = await api.get(`/movie/${id}?api_key=${api_key}`)
     dispatch(selectMovie(res.data))
+    
   }
-  useEffect(
-    () => {
-      if (id) {
-        getDetails()
-      }
-      return () => dispatch(removeMovie({}))
-    }, []
-  )
+  useEffect(()=>{
+    if(id){
+      detailsMovie()
+    }
+    return ()=>{
+      dispatch(removeMovie({}))
+    }
+  },[])
+let movie =useSelector((state)=>state.movies.movie);
 
-  let movie = {};
-  movie = useSelector((state) => state.movies.movie)
+
   return (
-    <div className='details_one'>
+    <div>
       {
-         JSON.stringify(movie) == {} ?
-          <div className='spinner'>
+        <div className='details' key={movie.id}>
 
-          </div> 
-          :
+         <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="images" />
+         <div>
+          <h1>{movie.title}</h1>
+          <h2>{movie.overview} </h2>
+          <h2> <i className="fa-solid fa-calendar-days"></i> {movie.release_date}</h2>
+          <h2> <i className="fa-solid fa-star-of-life"></i> {movie.vote_average} </h2>
+          <h2>{movie.tagline} </h2>
+         <NavLink to = {`/Video/${movie.id}`}>
+           <button type='button'> Watch</button>
+         </NavLink>
+         </div>
 
-          <div className='details_two'>
-
-             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" className='detailsImg' />
-            
-            <div>
-              <h1>{movie.title}</h1>
-              <p>{movie.overview}</p>
-              <h1> <i className="fa-solid fa-money-check-dollar"></i> Budget {movie.budget}</h1>
-              <h1> <i className="fa-regular fa-thumbs-up"></i> Popularity {movie.popularity}</h1>
-              <h1> <i className="fa-solid fa-calendar"></i> Relase Date {movie.release_date}</h1>
-              <h1> <i className="fa-solid fa-star"></i> {movie.vote_average}</h1>
-                 <NavLink to ={`/Video/${movie.id}`}>
-                <button type='button'>Watch</button>
-              </NavLink>
-              <h1> {movie.tagline}</h1>
-            </div>
-          </div>
+        </div>
       }
     </div>
   )
